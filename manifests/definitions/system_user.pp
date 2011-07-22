e8f89fccfa394de1b53e87040106668b64bb423d
Alfred Chan <alfredwchan@gmail.com>
2011-07-22 19:17:58 -0400

    Require rvm::system class instead of exec resource.

--- manifests/definitions/system_user.pp
+++ manifests/definitions/system_user.pp
@@ -8,6 +8,6 @@ define rvm::system_user () {
 
     exec { "/usr/sbin/usermod -a -G $group $username":
         unless => "/bin/cat /etc/group | grep $group | grep $username",
-        require => [User[$username], Exec['system-rvm']];
+        require => [User[$username], Class['rvm::system']];
     }
 }
