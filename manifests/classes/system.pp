f3cf5ac30d4d25e14302d96f2f720e5709e30e5f
Alfred Chan <alfredwchan@gmail.com>
2011-07-22 19:12:19 -0400

    Fix fact and install rvm only if it's not installed yet.

--- manifests/classes/system.pp
+++ manifests/classes/system.pp
@@ -2,13 +2,15 @@ class rvm::system {
 
   include rvm::dependencies
 
-  exec { 'system-rvm':
-    path    => '/usr/bin:/usr/sbin:/bin',
-    command => 'bash -c \'bash <(/usr/bin/curl -s https://rvm.beginrescueend.com/install/rvm)\'',
-    creates => '/usr/local/bin/rvm',
-    require => [
-      Class['rvm::dependencies'],
-    ],
+  if $rvm_installed != 'true' {  # only run if not installed yet
+    exec { 'system-rvm':
+      path    => '/usr/bin:/usr/sbin:/bin',
+      command => 'bash -c \'bash <(/usr/bin/curl -s https://rvm.beginrescueend.com/install/rvm)\'',
+      creates => '/usr/local/bin/rvm',
+      require => [
+        Class['rvm::dependencies'],
+      ],
+    }
   }
 
 }
