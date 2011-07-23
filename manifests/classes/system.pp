f6eebb2678ed12e775aad5b262033736c16a1d8b
Alfred Chan <alfredwchan@gmail.com>
2011-07-23 01:25:47 -0400

    Fix rvm binary path.

--- manifests/classes/system.pp
+++ manifests/classes/system.pp
@@ -2,15 +2,15 @@ class rvm::system {
 
   include rvm::dependencies
 
-  if $rvm_installed != 'true' {  # only run if not installed yet
+#  if $rvm_installed != 'true' {  # only run if not installed yet
     exec { 'system-rvm':
       path    => '/usr/bin:/usr/sbin:/bin',
       command => 'bash -c \'bash <(/usr/bin/curl -s https://rvm.beginrescueend.com/install/rvm)\'',
-      creates => '/usr/local/bin/rvm',
+      creates => '/usr/local/rvm/bin/rvm',
       require => [
         Class['rvm::dependencies'],
       ],
     }
-  }
+#  }
 
 }
