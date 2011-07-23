be5333f4a705cf49e136cc5d0038497f4e274126
Alfred Chan <alfredwchan@gmail.com>
2011-07-23 01:30:12 -0400

    Remove commented out lines (the check for rvm is done by "creates").

--- manifests/classes/system.pp
+++ manifests/classes/system.pp
@@ -2,15 +2,13 @@ class rvm::system {
 
   include rvm::dependencies
 
-#  if $rvm_installed != 'true' {  # only run if not installed yet
-    exec { 'system-rvm':
-      path    => '/usr/bin:/usr/sbin:/bin',
-      command => 'bash -c \'bash <(/usr/bin/curl -s https://rvm.beginrescueend.com/install/rvm)\'',
-      creates => '/usr/local/rvm/bin/rvm',
-      require => [
-        Class['rvm::dependencies'],
-      ],
-    }
-#  }
+  exec { 'system-rvm':
+    path    => '/usr/bin:/usr/sbin:/bin',
+    command => 'bash -c \'bash <(/usr/bin/curl -s https://rvm.beginrescueend.com/install/rvm)\'',
+    creates => '/usr/local/rvm/bin/rvm',
+    require => [
+      Class['rvm::dependencies'],
+    ],
+  }
 
 }
