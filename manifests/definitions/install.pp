56e6c481683f325e2abd797deafe009f51da8aa0
Alfred Chan <alfredwchan@gmail.com>
2011-07-27 05:39:33 -0400

    Add braces around variable.

--- manifests/definitions/install.pp
+++ manifests/definitions/install.pp
@@ -3,7 +3,7 @@ define rvm::ruby::install($ruby = $name, $default_ruby = false) {
   include rvm::params
 
   exec { "rvm install ruby-${ruby}":
-    command => "$rvm::params::bin/rvm install ruby-${ruby}",
+    command => "${rvm::params::bin}/rvm install ruby-${ruby}",
     creates => "/usr/local/rvm/rubies/ruby-${ruby}",
     require => Class['rvm::system'],
     timeout => 0,  # disable timeout for compiling ruby
@@ -11,7 +11,7 @@ define rvm::ruby::install($ruby = $name, $default_ruby = false) {
 
   if $default_ruby {
     exec { "rvm --default use ruby-${ruby}":
-      command => "$rvm::params::bin/rvm --default use ruby-${ruby}",
+      command => "${rvm::params::bin}/rvm --default use ruby-${ruby}",
       unless => "rvm list default | grep 'ruby-${ruby}'",
       require => Exec["rvm install ruby-${ruby}"],
     }
