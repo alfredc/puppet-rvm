a19919eebf1e5a5d7c2bded50ce23e4435ece364
Alfred Chan <alfredwchan@gmail.com>
2011-07-26 23:14:30 -0400

    Fix resource name to avoid duplicates.

--- manifests/definitions/gem-install.pp
+++ manifests/definitions/gem-install.pp
@@ -7,7 +7,7 @@ define rvm::gem::install($gemname = $name, $ruby, $gemset = 'global') {
     default => "ruby-${ruby}@${gemset}",
   }
 
-  exec { "install-${gemname}":
+  exec { "${name}":
     path => "${rvm::params::bin}:${rvm::params::root}/rubies/ruby-${ruby}/bin",
     command => "rvm use ${ruby_with_gemset} && gem install ${gemname}",
     unless => "ls ${rvm::params::root}/gems/${ruby_with_gemset}/gems/ | grep '${gemname}-'",
